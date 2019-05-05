9d568d7ae0784c24c400f211098a394521208256
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-05-04 20:35:09 -0500

    Implemented user connection to posts with tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+	has_many :posts
+  	# Include default devise modules. Others available are:
+  	# :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates_presence_of :first_name, :last_name
+  	validates_presence_of :first_name, :last_name
 end
