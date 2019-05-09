b1099fea03dcdac8c77f08a22602611f1d76ce2b
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-05-08 23:03:22 -0500

    Implemented Enum status to show Approval Statuses for posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,13 @@
 @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
 		
 puts "1 user created"
+
+@admin_user = AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "Snow")
+
+puts "1 admin user created"
+
 100.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
+	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, status: 0)
 end
 
 puts "100 posts have been created"
